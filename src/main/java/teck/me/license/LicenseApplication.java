63254112b43ee533e1162a6ece8a1dfde4abae74
Ali D <alidinpanah81@gmail.com>
2023-11-20 21:53:14 +0330

    project setting

---
+++ src/main/java/teck/me/license/LicenseApplication.java
@@ -0,0 +1,13 @@
+package teck.me.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teck/me/license/LicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package teck.me.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
